a10434ab44f849981de468e5e3de0b0218fb1a45
zhanglei <86521074@qq.com>
2023-08-01 08:01:44 +0800

    采购运单-添加上次基础功能

--- ruoyi-ui/src/views/transportdocuments/purchase/index.vue
+++ ruoyi-ui/src/views/transportdocuments/purchase/index.vue
@@ -92,7 +92,7 @@
           plain
           icon="el-icon-download"
           size="mini"
-          @click="handleExport"
+          @click="handleImport"
           v-hasPermi="['transportdocuments:detail:export']"
         >导入</el-button>
       </el-col>
@@ -335,6 +335,36 @@
         <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
+
+    <!-- 运输单导入对话框 -->
+    <el-dialog :title="upload.title" :visible.sync="upload.open" width="400px" append-to-body>
+      <el-upload
+        ref="upload"
+        :limit="1"
+        accept=".xlsx, .xls"
+        :headers="upload.headers"
+        :action="upload.url + '?updateSupport=' + upload.updateSupport"
+        :disabled="upload.isUploading"
+        :on-progress="handleFileUploadProgress"
+        :on-success="handleFileSuccess"
+        :auto-upload="false"
+        drag
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip text-center" slot="tip">
+          <!-- <div class="el-upload__tip" slot="tip">
+            <el-checkbox v-model="upload.updateSupport" /> 是否更新已经存在的用户数据
+          </div> -->
+          <span>仅允许导入xls、xlsx格式文件。</span>
+          <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="importTemplate">下载模板</el-link>
+        </div>
+      </el-upload>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitFileForm">确 定</el-button>
+        <el-button @click="upload.open = false">取 消</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -342,6 +372,7 @@
 import { listDetail, getDetail, delDetail, addDetail, updateDetail } from "@/api/transportdocuments/detail";
 import { regionData, CodeToText, TextToCode } from "element-china-area-data"
 import { listMaterialData } from "@/api/masterdata/material";
+import { getToken } from "@/utils/auth";
 
 export default {
   name: "Detail",
@@ -443,6 +474,20 @@ export default {
       optionsMaterialName: [],
       listMaterialName: [],
       remoteLoadingMaterialName: false,
+      upload: {
+        // 是否显示弹出层（回款导入）
+        open: false,
+        // 弹出层标题（回款导入）
+        title: "",
+        // 是否禁用上传
+        isUploading: false,
+        // 是否更新已经存在的回款数据
+        updateSupport: 0,
+        // 设置上传的请求头部
+        headers: { Authorization: "Bearer " + getToken() },
+        // 上传的地址
+        url: process.env.VUE_APP_BASE_API + "/transportdocuments/detail/importData"
+      },
     };
   },
   created() {
@@ -649,6 +694,32 @@ export default {
         return item.materialId == selValue;
       });
       this.form.materialName = selMaterial.materialName; // 物料名称
+    },
+    /** 导入按钮操作 */
+    handleImport() {
+      this.upload.title = "回款导入";
+      this.upload.open = true;
+    },
+     /** 下载模板操作 */
+     importTemplate() {
+      this.download('/transportdocuments/detail/importTemplate', {}, `运输单导入模板_${new Date().getTime()}.xlsx`)
+    },
+    /** 文件上传中处理 */
+    handleFileUploadProgress(event, file, fileList) {
+      this.upload.isUploading = true;
+    },
+    /** 文件上传成功处理 */
+    handleFileSuccess(response, file, fileList) {
+      this.upload.open = false;
+      this.upload.isUploading = false;
+      this.$refs.upload.clearFiles();
+      this.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true });
+      this.getList();
+    },
+    // 
+    /** 提交上传文件 */
+    submitFileForm() {
+      this.$refs.upload.submit();
     },
   }
 };
